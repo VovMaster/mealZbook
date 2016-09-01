40b53cde136594fed1098ff902a10a76032fe060
Vladimir <antonsimeonvladimir@gmail.com>
2016-09-01 16:39:36 +0300

    update

---
+++ mealzbook project Lina/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.fade2').slick({
+	  dots: true,
+	  infinite: true,
+	  speed: 500,
+	  fade: true,
+	  /*cssEase: 'linear'*/
+	});
+	$(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+});

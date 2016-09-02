727f2878069dc8f160c4eb79415aaf4846bd554a
vladimir <antonsimeonvladimir@gmail.com>
2016-09-02 13:20:06 +0300

    гзвфеук

--- mealzbook project Lina/js/scripts.js
+++ mealzbook project Lina/js/scripts.js
@@ -12,4 +12,33 @@ $(document).ready(function() {
         slidesToShow: 5,
         slidesToScroll: 1
     });
+
+
+	var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+
+	new ScrollMagic.Scene({triggerElement: "#parallax1"}).setTween(".parallax-bg", {y: "100%", ease: Linear.easeNone}).addTo(controller);
+
+
+	var wow = null;
+	// Elements Animation
+	if($('.wow').length){
+		wow = new WOW(
+		  {
+			boxClass:     'wow',      // animated element css class (default is wow)
+			animateClass: 'animated', // animation css class (default is animated)
+			offset:       50,          // distance to the element when triggering the animation (default is 0)
+			mobile:       true,       // trigger animations on mobile devices (default is true)
+			live:         true       // act on asynchronously loaded content (default is true)
+		  }
+		);
+		wow.init();
+
+	}
+
+
+
+	$('.page-up').click(function(){
+		$('html, body').animate({ scrollTop: 0 }, 500);
+		return false
+	});
 });
